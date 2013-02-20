8e499702d2a5f320fbb17b0160e5f43a91c25b24
mglunafh <ufispb@gmail.com>
2013-02-20 17:43:56 +0300

    YouTubePlayer and GoogleMaps generation. modified WebView generation and added a transition rule for it

--- SimpleGeneration/Program.fs
+++ SimpleGeneration/Program.fs
@@ -24,6 +24,18 @@ let writeToFile fn (str : string) =
 let append (stringBuilder : System.Text.StringBuilder) (str : string) = ignore(stringBuilder.Append(str))
 let insert (stringBuilder : System.Text.StringBuilder) position (str : string) = ignore(stringBuilder.Insert(position, str))
 
+let youTubeOverrides = "	 
+	@Override
+	public void onInitializationFailure(Provider arg0,YouTubeInitializationResult arg1) {
+		Toast.makeText(this, \"Initialization Fail\", Toast.LENGTH_LONG).show();
+	}
+	 
+	@Override
+	public void onInitializationSuccess(Provider provider, YouTubePlayer player,boolean wasrestored) {
+		ytp = player;
+		Toast.makeText(this, \"Initialization  Success\", Toast.LENGTH_LONG).show();
+	}"
+
 let transformXml (path : string) = 
     let newXml = new System.Text.StringBuilder()
 
@@ -111,6 +123,7 @@ let createImplementation form =
 import android.os.Bundle;
 import android.view.View;\n")
 
+    let ifHaveYouTubeElement = false
     let onCreate = new System.Text.StringBuilder()
     append onCreate ("\n\n    /**
     * Called when the activity is first created.
@@ -122,12 +135,13 @@ import android.view.View;\n")
             
     let activity = System.Text.StringBuilder()
     append activity (currentImports.ToString())
-    append activity ("\npublic class " + form + " extends Activity {")
 
     let reader = XmlReader.Create(path + @"\res\layout\" + form + ".xml")
+    
     while (reader.Read ()) do 
         match reader.Name with
         |"Button" ->
+            append activity ("\npublic class " + form + " extends Activity {")
             let onClickName = reader.GetAttribute("android:onClick")
 
             if not (imports.ContainsKey("android.content.Intent")) then 
@@ -150,6 +164,8 @@ import android.view.View;\n")
             append activity "\n    }"
 
         |"WebView" ->
+            append activity ("\npublic class " + form + " extends Activity {")
+            
             if not (permissions.ContainsKey("Internet")) then 
                 append manifest "    <uses-permission android:name=\"android.permission.INTERNET\" />\n"
                 permissions.Add("Internet", "Internet")
@@ -157,14 +173,89 @@ import android.view.View;\n")
             if not (imports.ContainsKey("android.webkit.WebView")) then 
                 insert activity 0 "\nimport android.webkit.WebView;"
                 imports.Add("android.webkit.WebView", "android.webkit.WebView")
+            
+            append onCreate ("\n        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);")
 
-            append onCreate ("\n        WebView webView = (WebView) findViewById(R.id.webViewInfo);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl(\"http://www.lanit-tercom.ru\");") // сайт зашит в код (было необходимо для теста)
+            let readerTrans = XmlReader.Create(path + @"\Transition2.xml")
+            let id = reader.GetAttribute("android:id")
+            while ((readerTrans.Read()) && not (readerTrans.GetAttribute("id") = id )) do
+                ignore()
+           
+            match readerTrans.GetAttribute("type") with
+            |"url" -> 
+                let url = readerTrans.GetAttribute("value")
+                append onCreate ("\n        webView.loadUrl(\"" + url + "\");")
+
+            |"youtubeview" -> 
+                if not (imports.ContainsKey("android.webkit.WebChromeClient")) then 
+                    insert activity 0 "\nimport android.webkit.WebChromeClient;"
+                    imports.Add("android.webkit.WebChromeClient", "android.webkit.WebChromeClient")
+                let code = readerTrans.GetAttribute("value")
+                
+                append onCreate ("\n		webView.setWebChromeClient(new WebChromeClient() {});
+        String html = \"<iframe class=\\\"youtube-player\\\" style=\\\"border: 0; width: 100%; height: 95%; padding:0px; margin:0px\\\" id=\\\"ytplayer\\\" type=\\\"text/html\\\" src=\\\"http://www.youtube.com/embed/" + code + "?fs=0\\\" frameborder=\\\"0\\\">\\\n" + "</iframe>\\\n\";
+		webView.getSettings().setPluginsEnabled(true);
+	    webView.loadDataWithBaseURL(\"\", html,\"text/html\", \"UTF-8\", \"\");")
+
+            |"googlemapsview" ->
+                if not (imports.ContainsKey("android.webkit.WebChromeClient")) then 
+                    insert activity 0 "\nimport android.webkit.WebChromeClient;"
+                    imports.Add("android.webkit.WebChromeClient", "android.webkit.WebChromeClient")
+                append onCreate ("\n		webView.setWebChromeClient(new WebChromeClient() {});
+        String html = \\\"<iframe  style=\\\"width: 100%; height: 95%; frameborder=\\\"0\\\" scrolling=\\\"no\\\" marginheight=\\\"0\\\" \\\"marginwidth=\\\"0\\\" src=\\\"https://maps.google.com/maps?ie=UTF8&amp;ll=59.86809,29.862041&amp;spn=0.024215,0.084543&amp;t=h&amp;z=14&amp;output=embed\\\">\\\" + \\\"</iframe><br />\\\";
+        webView.loadDataWithBaseURL(\"\", html,\"text/html\", \"UTF-8\", \"\");")
+            |_ -> ()
+
+        |"com.google.android.youtube.player.YouTubePlayerView" ->
+            let ifHaveYouTubeElement = true
+            let id = reader.GetAttribute("android:id")
+            let readerTrans = XmlReader.Create(path + @"\Transition2.xml")
+            while ((readerTrans.Read()) && not (readerTrans.GetAttribute("id") = id )) do
+                ignore()
+
+            let videoCode = readerTrans.GetAttribute("value");
+
+            append activity ("\npublic class " + form + "extends YouTubeBaseActivity 
+ 	implements YouTubePlayer.OnInitializedListener,OnEditorActionListener {")
+
+            if not (permissions.ContainsKey("Internet")) then 
+                append manifest "    <uses-permission android:name=\"android.permission.INTERNET\" />\n"
+                permissions.Add("Internet", "Internet")
+
+            let youTubeImports = ["com.google.android.youtube.player.YouTubeBaseActivity";
+                                  "com.google.android.youtube.player.YouTubeInitializationResult";
+                                  "com.google.android.youtube.player.YouTubePlayer";
+                                  "com.google.android.youtube.player.YouTubePlayer.Provider";
+                                  "com.google.android.youtube.player.YouTubePlayerView";
+                                  "android.widget.Toast"]
+
+            List.iter (fun import -> insert activity 0 ("\nimport " + import + ";")
+                                     imports.Add(import, import)) youTubeImports
+
+            append onCreate ("\n        		String developerKey = \"AI39si7GnhmZNIsKDvXYNDZwU0eOlTlWTJZ7zd0JmKoVVhCFzwfmSn6oI0yZDz8edJmDafwYgduM7IqerCF1yofdZlg-m2gluw\";
+        YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id." + id + ");
+		youTubePlayerView.initialize(developerKey, this);
+        youTubePlayerView.loadVideo(" + videoCode + ")")
+
+        |"com.google.android.maps.MapView" ->
+            let id = reader.GetAttribute("android:id")
+            let googleMapsImports = ["com.google.android.maps.MapActivity";
+                                     "com.google.android.maps.MapView";
+                                     ""]
+            List.iter (fun import -> insert activity 0 ("\nimport " + import + ";")
+                                     imports.Add(import, import) ) googleMapsImports
+
+            append activity ("\npublic class " + form + " extends MapActivity {")
+            append onCreate ("\n        MapView mapView = (MapView) findViewById(R.id." + id + ");      
+        mapView.setBuiltInZoomControls(true);")
+
         |_ -> ()
-    append onCreate "\n    }"
-    append activity (onCreate.ToString())
 
+    append onCreate "\n    }"
+    append onCreate (if ifHaveYouTubeElement then youTubeOverrides else "")
+
+    append activity (onCreate.ToString())
     append activity "\n}"
 
     insert activity 0 ("package com.qrealclouds." + package + ";\n")
@@ -195,6 +286,6 @@ let createApk =
     let pathToAndroidSdk = @"D:\android-sdk\sdk\tools\" //для работы на другом компе нужно заменть путь до android sdk
     ignore(System.Diagnostics.Process.Start("cmd.exe", "/C " + pathToAndroidSdk + createBuildXml))
     Thread.Sleep(1000);
-    ignore(System.Diagnostics.Process.Start("cmd.exe", "/C " + "cd /d " + path + " & ant debug"))
+    ignore(System.Diagnostics.Process.Start("cmd.exe", "/C " + "cd d" + path + " & ant debug")) 
 
 createApk
